9b2d66b79886024c2e616ddf21b7a7b58710d4a7
Petr Broz <petr.broz@autodesk.com>
2024-04-11 16:20:52 +0200

    Moved to official APS SDK.

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const { AuthenticationClient } = require('forge-server-utils');
-
+const { SdkManagerBuilder } = require('@aps_sdk/autodesk-sdkmanager');
+const { AuthenticationClient, Scopes } = require('@aps_sdk/authentication');
 const { APS_CLIENT_ID, APS_CLIENT_SECRET } = process.env;
 
-let authClient = new AuthenticationClient(APS_CLIENT_ID, APS_CLIENT_SECRET);
+const sdkManager = SdkManagerBuilder.create().build();
+const authenticationClient = new AuthenticationClient(sdkManager);
+
+let _credentials = null;
+async function getAccessToken() {
+    if (!_credentials || _credentials.expires_at < Date.now()) {
+        _credentials = await authenticationClient.getTwoLeggedToken(APS_CLIENT_ID, APS_CLIENT_SECRET, [Scopes.ViewablesRead]);
+        _credentials.expires_at = Date.now() + _credentials.expires_in * 1000;
+    }
+    return _credentials;
+}
+
 let router = express.Router();
 
 // GET /api/auth/token - provides an access token to be used by the Viewer
 router.get('/api/auth/token', async (req, res) => {
     try {
-        res.json(await authClient.authenticate(['viewables:read']));
+        const credentials = await getAccessToken();
+        res.json(credentials);
     } catch(err) {
         console.error(err);
         res.status(500).send(err.message);
